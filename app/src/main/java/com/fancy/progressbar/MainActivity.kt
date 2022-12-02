788782732297d4d9bc4d533b54157a5f916f7936
fatemeafashari <fati_afshari@yahoo.com>
2022-12-03 00:50:38 +0330

    edit example

--- app/src/main/java/com/fancy/progressbar/MainActivity.kt
+++ app/src/main/java/com/fancy/progressbar/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fancy.progressbar.ui.theme.FancyProgressbarTheme
@@ -24,7 +27,23 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
                     Box(modifier = Modifier.padding(horizontal = 16.dp, vertical = 32.dp)) {
-                        FancyProgressbar(initialValue = 0f, targetValue = 50f)
+                        FancyProgressbar(
+                            initialValue = 0f, // 0 to maxValue
+                            targetValue = 50f, // 0 to maxValue
+                            trackColor = Color(0xFFBFBFC0),
+                            indicatorColor = Brush.horizontalGradient(
+                                tileMode = TileMode.Clamp,
+                                colors = listOf(
+                                    Color(0xFFE91E63),
+                                    Color(0xFFF48FB1),
+                                )
+                            ),
+                            shadowColor = Color(0xFF7E57C2),
+                            radius = 16.dp,
+                            height = 16.dp,
+                            animationDuration = 3000,
+                            maxValue = 100f,
+                        )
                     }
                 }
             }
